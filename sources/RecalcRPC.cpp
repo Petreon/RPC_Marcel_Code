351fd7293934494f1c75398bdd4d43b955812b9c
Petreon <vitorr2009@gmail.com>
2024-05-18 17:49:28 -0300

    PV -Added img2terrain file and preparing RecalcRPC function

--- sources/RecalcRPC.cpp
+++ sources/RecalcRPC.cpp
@@ -1,4 +1,13 @@
 // Include libraries and functions
+#include "../includes/Eigen/Dense"
+#include "Affine.cpp"
+#include <iostream>
+
+typedef struct RecalcReturn
+{
+    Eigen::MatrixXd new_a;
+    Eigen::MatrixXd new_c;
+} RecalcReturn;
 
 // functions
 void RecalcRPC() {
