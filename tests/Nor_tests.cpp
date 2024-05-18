4dd5bb9d2e719bbefa97de11af821ee5d26ed462
Petreon <vitorr2009@gmail.com>
2024-05-18 11:34:46 -0300

    Added a test example and include eigen library

--- tests/Nor_tests.cpp
+++ tests/Nor_tests.cpp
@@ -1,4 +1,26 @@
 // Inlcude Nor
-
+#include <iostream>
+#include "../includes/Eigen/Dense"
 // use the function nor with Asserts librarie, if we change this project to CMAKE we will use, CTEST probably
 // but in the init use this way
+
+using Eigen::MatrixXd;
+
+/*
+HOW TO COMPILE THIS PROGRAM
+COMAND
+g++ -g -Wall -I ..\includes\Eigen\Eigen .\Nor_tests.cpp -o teste1
+maybe this is temporary
+*/
+
+int main()
+{
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
+
+    return 0;
+}
