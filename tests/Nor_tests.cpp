5544d2faf2a76c7f8746b999367577652fb584a3
Petreon <vitorr2009@gmail.com>
2024-05-18 11:43:55 -0300

    Added example using a sources function

--- tests/Nor_tests.cpp
+++ tests/Nor_tests.cpp
@@ -1,6 +1,7 @@
 // Inlcude Nor
 #include <iostream>
 #include "../includes/Eigen/Dense"
+#include "../sources/Nor.cpp"
 // use the function nor with Asserts librarie, if we change this project to CMAKE we will use, CTEST probably
 // but in the init use this way
 
@@ -9,12 +10,14 @@ using Eigen::MatrixXd;
 /*
 HOW TO COMPILE THIS PROGRAM
 COMAND
-g++ -g -Wall -I ..\includes\Eigen\Eigen .\Nor_tests.cpp -o teste1
+g++ -g -Wall -I ..\includes\Eigen\Eigen ..\sources\*.cpp .\Nor_tests.cpp -o teste1
 maybe this is temporary
 */
 
 int main()
 {
+    Normalization(); // Nor.cpp function
+
     MatrixXd m(2, 2);
     m(0, 0) = 3;
     m(1, 0) = 2.5;
