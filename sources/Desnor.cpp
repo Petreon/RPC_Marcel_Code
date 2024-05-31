206b0557e4d6de22314bcd53576a6347ab7a8c21
Arthur <arthurfernandes893@gmail.com>
2024-05-30 21:16:20 -0300

    ARTHUR - Creation of Desnor Function and ajustments in Nor function, some errors still remaining

--- sources/Desnor.cpp
+++ sources/Desnor.cpp
@@ -1,6 +1,21 @@
 // Include libraries and functions
-
+#include "../includes/Eigen/Dense"
+#include <iostream>
+#include "Nor.cpp"
 // functions
-void Desnormalization() {
 
+long ElementDesnormalization(long normalaizedValue, long OffSetFactor, long ScaleFactor) {
+    return (normalaizedValue*ScaleFactor)+OffSetFactor;
 };
+
+Eigen::MatrixXd ImgDesnormalization(Eigen::MatrixXd img, CorrectionValues desnorCorrection[]){
+    int row = img.rows();
+    int col = img.cols();
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            img(i,j) = ElementDesnormalization(img(i,j),desnorCorrection[0].off,desnorCorrection[0].scale);
+        }
+    }
+    return img;
+}
