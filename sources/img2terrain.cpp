f55318c9ff6baf2f128b2a1fee33fe989f9c0d1f
Arthur <arthurfernandes893@gmail.com>
2024-07-15 13:28:39 -0300

    change in parameters definition

--- sources/img2terrain.cpp
+++ sources/img2terrain.cpp
@@ -24,7 +24,7 @@ function [latitude, longitude, altitude] = img2terrain(Line1, Sample1, Line2, Sa
 
 */
 
-Coordinates SpacialInterssection(Eigen::MatrixXd Line1, Eigen::MatrixXd Sample1, Eigen::MatrixXd Line2, Eigen::MatrixXd Sample2, CorrectionValues l1Correction, CorrectionValues s1Correction, CorrectionValues l2Correction, CorrectionValues s2Correction, CorrectionValues latCorrection, CorrectionValues longCorrection, CorrectionValues hCorrection){
+Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoefImg1, Eigen::MatrixXd cCoefImg1, Eigen::MatrixXd dCoefImg1, Eigen::MatrixXd aCoefImg2, Eigen::MatrixXd bCoefImg2, Eigen::MatrixXd cCoefImg2, Eigen::MatrixXd dCoefImg2 Eigen::MatrixXd Line1, Eigen::MatrixXd Sample1, Eigen::MatrixXd Line2, Eigen::MatrixXd Sample2, CorrectionValues l1Correction, CorrectionValues s1Correction, CorrectionValues l2Correction, CorrectionValues s2Correction, CorrectionValues latCorrection, CorrectionValues longCorrection, CorrectionValues hCorrection){
 
 // Normalization of Line and Sample  Values for both images
     Eigen::MatrixXd l1 = Normalization(Line1, l1Correction);
