55dd8f634b399d7e300bde6e0dc0d38cc1940324
Petreon <vitorr2009@gmail.com>
2024-07-16 20:59:00 -0300

    PV - Created latlong2utm_tests and initialized building latlong2utm function

---
+++ tests/latlong2utm_tests.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "../includes/Eigen/Dense"
+#include "../sources/latlong2utm.cpp"
+// use the function nor with Asserts libraries, if we change this project to CMAKE we will use CTEST probably
+// but in the init use this way
+
+// g++ -g -Wall -I ..\includes\Eigen\Eigen .\latlong2utm_tests.cpp -o latlong2utm_tests
+// values for testing
+/* lat
+-22.914
+-22.985
+-22.913
+-22.924
+*/
+/* long
+-43.206
+-43.198
+-43.227
+-43.236
+*/
+
+int main()
+{
+    Eigen::MatrixXd latitute(4, 1);
+    latitute << -22.914,
+        -22.985,
+        -22.913,
+        -22.924;
+
+    Eigen::MatrixXd longitute(4, 1);
+    longitute << -43.206,
+        -43.198,
+        -43.227,
+        -43.236;
+
+    std::cout << latitute << std::endl;
+    std::cout << "_______________" << std::endl;
+    std::cout << longitute << std::endl;
+    std::cout << "_______________" << std::endl;
+
+    LatLong2UTM(latitute, longitute);
+    // CoordinatesUTM coordinatesConverted = LatLong2UTM(latitute, longitute);
+    return 0;
+}
