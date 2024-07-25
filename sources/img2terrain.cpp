1f18a08eba252a3036f89ea2d2515c392c4e8cc7
Arthur Henrique de Sousa Fernandes <arthur.fernandes@planejamento.rj.gov.br.local>
2024-07-25 14:52:35 -0300

    ARTHUR - development of iterative method

--- sources/img2terrain.cpp
+++ sources/img2terrain.cpp
@@ -24,7 +24,11 @@ function [latitude, longitude, altitude] = img2terrain(Line1, Sample1, Line2, Sa
 
 */
 
-Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoefImg1, Eigen::MatrixXd cCoefImg1, Eigen::MatrixXd dCoefImg1, Eigen::MatrixXd aCoefImg2, Eigen::MatrixXd bCoefImg2, Eigen::MatrixXd cCoefImg2, Eigen::MatrixXd dCoefImg2 Eigen::MatrixXd Line1, Eigen::MatrixXd Sample1, Eigen::MatrixXd Line2, Eigen::MatrixXd Sample2, CorrectionValues l1Correction, CorrectionValues s1Correction, CorrectionValues l2Correction, CorrectionValues s2Correction, CorrectionValues latCorrection1, CorrectionValues longCorrection1, CorrectionValues hCorrection1, CorrectionValues latCorrection2, CorrectionValues longCorrection2, CorrectionValues hCorrection2){
+Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoefImg1, Eigen::MatrixXd cCoefImg1, Eigen::MatrixXd dCoefImg1, Eigen::MatrixXd aCoefImg2, 
+                                Eigen::MatrixXd bCoefImg2, Eigen::MatrixXd cCoefImg2, Eigen::MatrixXd dCoefImg2, Eigen::MatrixXd Line1, Eigen::MatrixXd Sample1, 
+                                Eigen::MatrixXd Line2, Eigen::MatrixXd Sample2, CorrectionValues l1Correction, CorrectionValues s1Correction, CorrectionValues l2Correction, 
+                                CorrectionValues s2Correction, CorrectionValues latCorrection1, CorrectionValues longCorrection1, CorrectionValues hCorrection1, 
+                                CorrectionValues latCorrection2, CorrectionValues longCorrection2, CorrectionValues hCorrection2){
 
 // Normalization of Line and Sample  Values for both images
     Eigen::MatrixXd l1 = Normalization(Line1, l1Correction);
@@ -38,7 +42,14 @@ Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoe
     to aproximate initial values for the line and collumn of the pixel in the image.
  */
 
-    //LINE POLINOMIAL
+    //LINE (l) POLINOMIAL
+    /*
+    a and b correspond to the coeficients of 
+    the numerator and denominator respectively
+
+    At the end, line should be
+    l = (a0_Img1 + a1_Img1 + a2_Img1 + a3_Img1) / (b0_Img1 + b1_Img1 + b2_Img1 + b3_Img1)
+    */
 
     a0_Img1 =   aCoefImg1[0] * longCorrection1.scale * latCorrection1.scale * hCorrection1.scale - 
                 aCoefImg1[1] * longCorrection1.off * latCorrection1.scale * hCorrection1.scale - 
@@ -63,7 +74,15 @@ Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoe
 
     b3_Img1 =   bCoefImg1[3] * longCorrection1.scale * latCorrection1.scale;
 
-    //COLLUMN POLINOMIAL
+    //COLLUMN (s) POLINOMIAL
+    /*
+    c and d correspond to the coeficients of 
+    the numerator and d=enominator respectively
+
+    At the end, collumn should be
+    s = (c0_Img2 + c1_Img2 + c2_Img2 + c3_Img2) / (d0_Img2 + d1_Img2 + d2_Img2 + d3_Img2)
+    */
+
     c0_Img1 =   cCoefImg1[0] * longCorrection1.scale * latCorrection1.scale * hCorrection1.scale - 
                 cCoefImg1[1] * longCorrection1.off * latCorrection1.scale * hCorrection1.scale - 
                 cCoefImg1[2] * longCorrection1.scale * latCorrection1.off * hCorrection1.scale -
@@ -87,9 +106,28 @@ Coordinates SpacialInterssection(Eigen::MatrixXd aCoefImg1, Eigen::MatrixXd bCoe
 
     d3_Img1 =   dCoefImg1[3] * longCorrection1.scale * latCorrection1.scale;
 
+    
+    /*
+	A = [   a1linha1 - l1(i)*b1linha1,      a2linha1 - l1(i)*b2linha1,      a3linha1 - l1(i)*b3linha1;         
+            c1linha1 - s1(i)*d1linha1,      c2linha1 - s1(i)*d2linha1,      c3linha1 - s1(i)*d3linha1;         
+            a1linha2 - l2(i)*b1linha2,      a2linha2 - l2(i)*b2linha2,      a3linha2 - l2(i)*b3linha2;         
+            c1linha2 - s2(i)*d1linha2,      c2linha1 - s2(i)*d2linha2,      c3linha1 - s2(i)*d3linha2];  
+
+
+	L = [   l1(i)*b0linha1 - a0linha1; 
+            s1(i)*d0linha1 - c0linha1; 
+            l2(i)*b0linha2 - a0linha2; 
+            s2(i)*d0linha2 - c0linha2];
+
+	[X,V] = mmq(A,L);
+	Xa = [Xa; X];  
+    */
+    Eigen::MatrixXd<double,4,3> A;
+    Eigen::MatrixXd<double,4,1> L;
+    
     for (int i = 0; i < Line1.rows(); i++)
     {
-        
+           
     }
     
 }
