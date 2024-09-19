f572077bd2bd9397feadf63a241166de03d54d80
Arthur <arthurfernandes893@gmail.com>
2024-09-18 22:20:26 -0300

    ARTHUR - indexes bugs fixed

--- sources/img2terrain.cpp
+++ sources/img2terrain.cpp
@@ -24,16 +24,16 @@ it returns three matrixes, each for a coordinate, and in the position of the pix
  - altitude
 
 does this octave operation:
-	function [latitude, longitude, altitude] = img2terrain(Line1, Sample1, Line2, Sample2, a1, b1, c1, d1, a2, b2 ,c2 ,d2, Line_off1, Sample_off1, Lat_off1, Long_off1, H_off1, Line_scale1, Sample_scale1, Lat_scale1, Long_scale1, H_scale1, Line_off2, Sample_off2, Lat_off2, Long_off2, H_off2, Line_scale2, Sample_scale2, Lat_scale2, Long_scale2, H_scale2) 
+    function [latitude, longitude, altitude] = img2terrain(Line1, Sample1, Line2, Sample2, a1, b1, c1, d1, a2, b2 ,c2 ,d2, Line_off1, Sample_off1, Lat_off1, Long_off1, H_off1, Line_scale1, Sample_scale1, Lat_scale1, Long_scale1, H_scale1, Line_off2, Sample_off2, Lat_off2, Long_off2, H_off2, Line_scale2, Sample_scale2, Lat_scale2, Long_scale2, H_scale2) 
 */
 
 Coordinates SpacialInterssection(Coeficients Coefs_Img1, Coeficients Coefs_Img2, Eigen::MatrixXd Line1, Eigen::MatrixXd Sample1, Eigen::MatrixXd Line2, Eigen::MatrixXd Sample2, CorrectionValues l1Correction, CorrectionValues s1Correction, CorrectionValues l2Correction, CorrectionValues s2Correction, CorrectionValues latCorrection1, CorrectionValues longCorrection1, CorrectionValues hCorrection1, CorrectionValues latCorrection2, CorrectionValues longCorrection2, CorrectionValues hCorrection2){
 
 // Normalization of Line and Sample  Values for both images
     Eigen::MatrixXd l1 = Normalization(Line1, l1Correction);
-    Eigen::MatrixXd s1 = Normalization(Line1, s1Correction);
-    Eigen::MatrixXd l2 = Normalization(Line1, l2Correction);
-    Eigen::MatrixXd s2 = Normalization(Line1, s2Correction);
+    Eigen::MatrixXd s1 = Normalization(Sample1, s1Correction);
+    Eigen::MatrixXd l2 = Normalization(Line2, l2Correction);
+    Eigen::MatrixXd s2 = Normalization(Sample2, s2Correction);
     
 
 /*
@@ -42,7 +42,7 @@ to aproximate initial values for the line and collumn of the pixel in both image
 - l indicates the LINE Polinomial and s the COLLUMN Polinomial.
 -   At the end, line and collumn should be:
         l = (a0_Img1 + a1_Img1 + a2_Img1 + a3_Img1) / (b0_Img1 + b1_Img1 + b2_Img1 + b3_Img1)
-	s = (c0_Img2 + c1_Img2 + c2_Img2 + c3_Img2) / (d0_Img2 + d1_Img2 + d2_Img2 + d3_Img2)
+    s = (c0_Img2 + c1_Img2 + c2_Img2 + c3_Img2) / (d0_Img2 + d1_Img2 + d2_Img2 + d3_Img2)
 */
 
     double a0_Img1 =   Coefs_Img1.a(0,0) * longCorrection1.scale * latCorrection1.scale * hCorrection1.scale - 
@@ -68,7 +68,7 @@ to aproximate initial values for the line and collumn of the pixel in both image
 
     double b3_Img1 =   Coefs_Img1.b(3,0) * longCorrection1.scale * latCorrection1.scale;
 
-	
+    
     double c0_Img1 =   Coefs_Img1.c(0,0) * longCorrection1.scale * latCorrection1.scale * hCorrection1.scale - 
                                 Coefs_Img1.c(1,0) * longCorrection1.off * latCorrection1.scale * hCorrection1.scale - 
                                 Coefs_Img1.c(2,0) * longCorrection1.scale * latCorrection1.off * hCorrection1.scale -
@@ -160,9 +160,9 @@ obs: Static size matrixes were used to optimeze performance.
               c1_Img2-s2(i,0)*d1_Img2, c2_Img2-s2(i,0)*d2_Img2, c3_Img2-s2(i,0)*d3_Img2;
 
         L << l1(i,0)*b0_Img1 - a0_Img1, 
-             s1(0,0)*d0_Img1 - c0_Img1,
+             s1(i,0)*d0_Img1 - c0_Img1,
              l2(i,0)*b0_Img2 - a0_Img2, 
-             s2(0,0)*d0_Img2 - c0_Img2;
+             s2(i,0)*d0_Img2 - c0_Img2;
 
         Xa = mmq(A,L).Xa;
     }
@@ -178,9 +178,9 @@ obs: Static size matrixes were used to optimeze performance.
     Eigen::MatrixXd Pontosh(Line1.rows(),1);
     
     for (int i = 0; i < Line1.rows(); i++){
-        PontosLong(i,1) = 3*i-2;
-        PontosLat(i,1) = 3*i-1;
-        Pontosh(i,1) = 3*i;
+        PontosLong(i,0) = 3*i-2;
+        PontosLat(i,0) = 3*i-1;
+        Pontosh(i,0) = 3*i;
     }
 
     Eigen::MatrixXd Long = Xa(PontosLong,Eigen::all);
