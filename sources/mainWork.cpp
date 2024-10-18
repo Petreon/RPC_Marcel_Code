bfe750f32cd643dfeaa18cf69681fe0ab9c193cb
Paulo Vitor <vitorr2009@gmail.com>
2024-10-18 15:53:18 -0300

    PV - Started SpartialIntersection points in mainWork

--- sources/mainWork.cpp
+++ sources/mainWork.cpp
@@ -9,15 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_ARQUIVOS 4
-#define LINHAS_POR_ARQUIVO 20
-
-#define NUM_ARQUIVOS2 3
-#define LINHAS_POR_ARQUIVO2 36
-
-#define NUM_ARQUIVOS3 2
-#define LINHAS_POR_ARQUIVO3 36
-
 
 
 //2 PARÂMETROS DA IMAGEM (OFFSET E SCALE) - Presente nos arquivos .RPB de metadados da imagem
@@ -46,12 +37,12 @@
 #define HEIGHT_OFF2     225   
 #define HEIGHT_SCALE2   500;
 
-//For the 1° image
+//RPCS For the 1° image
 Eigen::MatrixXd a1(20, 1);
 Eigen::MatrixXd b1(20, 1);
 Eigen::MatrixXd c1(20, 1);
 Eigen::MatrixXd d1(20, 1);
-//for the 2° image
+//RPCs for the 2° image
 Eigen::MatrixXd a2(20, 1);
 Eigen::MatrixXd b2(20, 1);
 Eigen::MatrixXd c2(20, 1);
@@ -77,7 +68,7 @@ int main(int argc, char* argv[]){
     //Control Points in Coordinates
     Eigen::MatrixXd B1_pc(AvailablePoints, 1); //This is variable depending on how many point disponible you have
     Eigen::MatrixXd L1_pc(AvailablePoints, 1);
-    Eigen::MatrixXd H1_pc(AvailablePoints,1);
+    Eigen::MatrixXd H1_pc(AvailablePoints, 1);
 
     Eigen::MatrixXd B2_pc(AvailablePoints, 1);
     Eigen::MatrixXd L2_pc(AvailablePoints, 1);
@@ -85,10 +76,14 @@ int main(int argc, char* argv[]){
 
     //Control Points in Pixel
     Eigen::MatrixXd Line1(AvailablePoints, 1);
-    Eigen::MatrixXd Sample1(AvailablePoints, 1); // in reality this is Colmun pixel of the control point in the image
+    Eigen::MatrixXd Sample1(AvailablePoints, 1); // in reality this is Colmun pixel of the controls point in the image, 
 
     Eigen::MatrixXd Line2(AvailablePoints, 1);
-    Eigen::MatrixXd Sample2(AvailablePoints, 1); // in reality this is Colmun pixel of the control point in the image //TODO: Change to column
+    Eigen::MatrixXd Sample2(AvailablePoints, 1); // in reality this is Colmun pixel of the controls point in the image //TODO: Change to column
+
+
+    Coeficients Coef_Img1, Coef_Img2, Coef_Img1_new, Coef_Img2_new; //Structure used to SpacialInterssection function
+    CorrectionValues l1Correction, s1Correction, l2Correction, s2Correction, latCorrection1, longCorrection1, hCorrection1, latCorrection2, longCorrection2, hCorrection2; //Structure used to SpacialInterssection function
 
 
     for (int i = 0; i < runTimes; i++){
@@ -104,35 +99,70 @@ int main(int argc, char* argv[]){
         //Obs MatrixSubSetting function is the same as this sintaxe in octave B1_pc(IndexTrain,:)
         //[new_a1, new_c1] = RecalcRPC(B1_pc(IndexTrain, :), L1_pc(IndexTrain, :), H1_pc(IndexTrain, :), a1, b1, c1, d1, Line1(IndexTrain, :), Sample1(IndexTrain, :), LINE_OFF1, LINE_SCALE1, SAMPLE_OFF1, SAMPLE_SCALE1);
         //[new_a2, new_c2] = RecalcRPC(B2_pc(IndexTrain, :), L2_pc(IndexTrain, :), H2_pc(IndexTrain, :), a2, b2, c2, d2, Line2(IndexTrain, :), Sample2(IndexTrain, :), LINE_OFF2, LINE_SCALE2, SAMPLE_OFF2, SAMPLE_SCALE2);
-        RecalcReturn new_a1_c1 = RecalcRPC(MatrixSubSetting(B1_pc, IndexTrain), MatrixSubSetting(L1_pc, IndexTrain), MatrixSubSetting(H1_pc, IndexTrain), a1, b1, c1, d1, MatrixSubSetting(Line1, IndexTrain), MatrixSubSetting(Sample1, IndexTrain), LINE_OFF1, LINE_SCALE1, SAMPLE_OFF1, SAMPLE_SCALE1);
+        RecalcReturn new_a1_c1 = RecalcRPC(MatrixSubSetting(B1_pc, IndexTrain), MatrixSubSetting(L1_pc, IndexTrain), MatrixSubSetting(H1_pc, IndexTrain), a1, b1, c1, d1, MatrixSubSetting(Line1, IndexTrain), MatrixSubSetting(Sample1, IndexTrain), LINE_OFF1, LINE_SCALE1, SAMPLE_OFF1, SAMPLE_SCALE1); //TODO: this macros need to change to variables to make it dinamically in the future
         RecalcReturn new_a2_c2 = RecalcRPC(MatrixSubSetting(B2_pc, IndexTrain), MatrixSubSetting(L2_pc, IndexTrain), MatrixSubSetting(H2_pc, IndexTrain), a2, b2, c2, d2, MatrixSubSetting(Line2, IndexTrain), MatrixSubSetting(Sample2, IndexTrain), LINE_OFF2, LINE_SCALE2, SAMPLE_OFF2, SAMPLE_SCALE2);
 
 
         // 5.3 Calculate the coordinates of terrain latitude, longitude and altitude
 
         // 5.3.1 - For the training points - before and after
-        Coeficients Coef_Img1, Coef_Img2, Coef_Img1_new, Coef_Img2_new;
         Coef_Img1.a = a1; Coef_Img1.b = b1; Coef_Img1.c = c1; Coef_Img1.d = d1;
-        Coef_Img2.a = a1; Coef_Img2.b = b1; Coef_Img2.c = c1; Coef_Img2.d = d1;
+        Coef_Img2.a = a2; Coef_Img2.b = b2; Coef_Img2.c = c2; Coef_Img2.d = d2;
 
         Coef_Img1_new.a = new_a1_c1.Calc_rpc_nl; Coef_Img1_new.b = b1; Coef_Img1_new.c = new_a1_c1.Calc_rpc_ns; Coef_Img1_new.d = d1;
-        Coef_Img2_new.a = a1; Coef_Img2_new.b = a1; Coef_Img2_new.c = a1; Coef_Img2_new.d = a1;
+        Coef_Img2_new.a = new_a2_c2.Calc_rpc_nl; Coef_Img2_new.b = b2; Coef_Img2_new.c = new_a2_c2.Calc_rpc_ns; Coef_Img2_new.d = d2;
         
+        //This part need to be variables instead macros, if they are constants and dont change in the code, it can be put outside the loop
+        l1Correction.scale      = LINE_SCALE1    ; l1Correction.off      = LINE_OFF1; 
+        s1Correction.scale      = SAMPLE_SCALE1  ; s1Correction.off      = SAMPLE_OFF1;
+        
+        latCorrection1.scale    = LAT_SCALE1     ; latCorrection1.off    = LAT_OFF1;
+        longCorrection1.scale   = LONG_SCALE1    ; longCorrection1.off   = LONG_OFF1;
+        hCorrection1.scale      = HEIGHT_SCALE1  ; hCorrection1.off      = HEIGHT_OFF1;
+        
+        //Structure for the second image
+        l2Correction.scale      = LINE_SCALE2    ; l2Correction.off      = LINE_OFF2;
+        s2Correction.scale      = SAMPLE_SCALE2  ; s2Correction.off      = SAMPLE_OFF2;
 
-        // Antes
+        latCorrection2.scale    = LAT_SCALE2     ; latCorrection2.off    = LAT_OFF2;
+        longCorrection2.scale   = LONG_SCALE2    ; longCorrection2.off   = LONG_OFF2;
+        hCorrection2.scale      = HEIGHT_SCALE2  ; hCorrection2.off      = HEIGHT_OFF2;
+
+        // Before
         //Coordinates Train_lat_long_h_before = SpacialInterssection(MatrixSubSetting(Line1, IndexTrain), MatrixSubSetting(Sample1, IndexTrain), MatrixSubSetting(Line2, IndexTrain), MatrixSubSetting(Sample2, IndexTrain), a1, b1, c1, d1, a2, b2, c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
-        Coordinates Train_lat_long_h_before = SpacialInterssection();
+        Coordinates Train_lat_long_h_before = SpacialInterssection(Coef_Img1, Coef_Img2, MatrixSubSetting(Line1, IndexTrain) , MatrixSubSetting(Sample1, IndexTrain),MatrixSubSetting(Line2, IndexTrain) , MatrixSubSetting(Sample2, IndexTrain), l1Correction, s1Correction, l2Correction, s2Correction, latCorrection1, longCorrection1, hCorrection1, latCorrection2, longCorrection2, hCorrection2);
 
-        // Depois
-        Coordinates Train_lat_long_h_after = SpacialInterssection(Line1,IndexTrain), Sample1,IndexTrain), Line2,IndexTrain), Sample2,IndexTrain), new_a1, b1, new_c1, d1, new_a2, b2, new_c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        // After
+        //Coordinates Train_lat_long_h_after = SpacialInterssection(Line1,IndexTrain), Sample1,IndexTrain), Line2,IndexTrain), Sample2,IndexTrain), new_a1, b1, new_c1, d1, new_a2, b2, new_c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        Coordinates Train_lat_long_h_after  = SpacialInterssection(Coef_Img1_new, Coef_Img2_new, MatrixSubSetting(Line1, IndexTrain) , MatrixSubSetting(Sample1, IndexTrain),MatrixSubSetting(Line2, IndexTrain) , MatrixSubSetting(Sample2, IndexTrain), l1Correction, s1Correction, l2Correction, s2Correction, latCorrection1, longCorrection1, hCorrection1, latCorrection2, longCorrection2, hCorrection2);
 
-        // 5.3.1 - Para os pontos de teste - antes e depois
+        // 5.3.1 - For the tests points - before and after
 
-        // Antes
-        Coordinates Tests_lat_long_h_before = SpacialInterssection(Line1,IndexTest), Sample1,IndexTest), Line2,IndexTest), Sample2,IndexTest), a1, b1, c1, d1, a2, b2, c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        // Before
+        //Coordinates Tests_lat_long_h_before = SpacialInterssection(Line1,IndexTest), Sample1,IndexTest), Line2,IndexTest), Sample2,IndexTest), a1, b1, c1, d1, a2, b2, c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        Coordinates Train_lat_long_h_before = SpacialInterssection(Coef_Img1, Coef_Img2, MatrixSubSetting(Line1, IndexTest) , MatrixSubSetting(Sample1, IndexTest),MatrixSubSetting(Line2, IndexTest) , MatrixSubSetting(Sample2, IndexTest), l1Correction, s1Correction, l2Correction, s2Correction, latCorrection1, longCorrection1, hCorrection1, latCorrection2, longCorrection2, hCorrection2);
 
-        // Depois
-        Coordinates Tests_lat_long_h_after = SpacialInterssection(Line1,IndexTest), Sample1,IndexTest), Line2,IndexTest), Sample2,IndexTest), new_a1, b1, new_c1, d1, new_a2, b2, new_c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        // After
+        //Coordinates Tests_lat_long_h_after = SpacialInterssection(Line1,IndexTest), Sample1,IndexTest), Line2,IndexTest), Sample2,IndexTest), new_a1, b1, new_c1, d1, new_a2, b2, new_c2, d2, LINE_OFF1, SAMPLE_OFF1, LAT_OFF1, LONG_OFF1, HEIGHT_OFF1, LINE_SCALE1, SAMPLE_SCALE1, LAT_SCALE1, LONG_SCALE1, HEIGHT_SCALE1, LINE_OFF2, SAMPLE_OFF2, LAT_OFF2, LONG_OFF2, HEIGHT_OFF2, LINE_SCALE2, SAMPLE_SCALE2, LAT_SCALE2, LONG_SCALE2, HEIGHT_SCALE2);
+        Coordinates Train_lat_long_h_after  = SpacialInterssection(Coef_Img1_new, Coef_Img2_new, MatrixSubSetting(Line1, IndexTest) , MatrixSubSetting(Sample1, IndexTest),MatrixSubSetting(Line2, IndexTest) , MatrixSubSetting(Sample2, IndexTest), l1Correction, s1Correction, l2Correction, s2Correction, latCorrection1, longCorrection1, hCorrection1, latCorrection2, longCorrection2, hCorrection2);
+
+        //6 Conversion of this coordinates to UTM. i think is UTM 24 - E, go into the latlong2utm function to know the 
+	
+	    //6.1 For the training points 
+	
+        // [Train_E_intersec_antes, Train_N_intersec_antes, Train_Fuso_intersec_antes] = latlong2utm(Train_lat_intersec_antes, Train_long_intersec_antes);
+        // [Train_E_intersec_depois, Train_N_intersec_depois, Train_Fuso_intersec_depois] = latlong2utm(Train_lat_intersec_depois, Train_long_intersec_depois);
+        
+        // % 6.2 Para os pontos de teste
+
+        // [Test_E_intersec_antes, Test_N_intersec_antes, Test_Fuso_intersec_antes] = latlong2utm(Test_lat_intersec_antes, Test_long_intersec_antes);
+        // [Test_E_intersec_depois, Test_N_intersec_depois, Test_Fuso_intersec_depois] = latlong2utm(Test_lat_intersec_depois, Test_long_intersec_depois);
+        
+        // % 6.3 Para os pontos de campo
+        
+        // [LAT_E, LONG_N, LAT_LONG_Fuso] = latlong2utm(LAT, LONG);
+
+
 
     }
 
